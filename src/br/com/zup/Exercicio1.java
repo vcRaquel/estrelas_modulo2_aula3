892fcd410f7468bde1f37abd8abd5d89f53f6b37
Raquel Leão <vc.raquel@gmail.com>
2021-08-26 11:38:36 -0300

    exercicio 1

---
+++ src/br/com/zup/Exercicio1.java
@@ -0,0 +1,20 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Por favor digite o primeiro número:");
+        double numero1 = leitor.nextDouble();
+        System.out.println("Por favor digite o segundo número:");
+        double numero2 = leitor.nextDouble();
+
+        if(numero1>numero2){
+            System.out.println("O primeiro número é maior");
+        }else {
+            System.out.println("O segundo número é maior");
+        }
+    }
+}
