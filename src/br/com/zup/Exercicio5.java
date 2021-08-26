e6a2d61f0bab7027afacc7c5d828170912ccadd6
Raquel Leão <vc.raquel@gmail.com>
2021-08-26 13:41:06 -0300

    exercicio 5

---
+++ src/br/com/zup/Exercicio5.java
@@ -0,0 +1,47 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite os seguintes valores para seus respectivos diascorrespondentes:");
+        System.out.println("1 - Para Segunda");
+        System.out.println("2 - Para Terça");
+        System.out.println("3 - Para Quarta");
+        System.out.println("4 - Para Quinta");
+        System.out.println("5 - Para Sexta");
+        System.out.println("6 - Para Sábado");
+        System.out.println("7 - Para Domingo");
+        System.out.println("Digite o valor desejado: ");
+        double diaDesejado = leitor.nextDouble();
+
+        if(diaDesejado > 0 & diaDesejado <= 7){
+            if (diaDesejado == 1){
+                System.out.println("O dia desejado foi a Segunda");
+            }
+            else if (diaDesejado == 2){
+                System.out.println("O dia desejado foi a Terça");
+            }
+            else if (diaDesejado == 3){
+                System.out.println("O dia desejado foi a Quarta");
+            }
+            else if (diaDesejado == 4){
+                System.out.println("O dia desejado foi a Quinta");
+            }
+            else if (diaDesejado == 5){
+                System.out.println("O dia desejado foi a Sexta");
+            }
+            else if (diaDesejado == 6){
+                System.out.println("O dia desejado foi a Sábado");
+            }
+            else if (diaDesejado == 7){
+                System.out.println("O dia desejado foi a Domingo");
+            }
+        }else{
+            System.out.println("Digite uma opção válida");
+        }
+    }
+}
